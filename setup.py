4d5015a21126448b2e68bc0fb1d3308d44d58ff2
Auyer <rafael@rcpassos.me>
2020-10-12 17:43:34 -0300

    updates libraries

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@
 
 from setuptools import setup
 
-setup(name = 'target-parquet',
-      version = '0.0.1',
-      description = 'Singer.io target for writing into parquet files',
-      author = 'Stitch',
-      url = 'https://singer.io',
-      classifiers = ['Programming Language :: Python :: 3 :: Only'],
-      py_modules = ['target_parquet'],
+setup(name='target-parquet',
+      version='0.1.1',
+      description='Singer.io target for writing into parquet files',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['target_parquet'],
       install_requires=[
           'jsonschema==2.6.0',
-          'singer-python==2.1.4',
-          'pandas==0.24.2',
-          'pyarrow==0.13.0'
+          'singer-python==5.9.0',
+          'pandas==1.1.3',
+          'pyarrow==1.0.1'
       ],
       entry_points='''
           [console_scripts]
           target-parquet=target_parquet:main
       ''',
-)
+      )
