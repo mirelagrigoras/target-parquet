1b5f74fefdc0d4ccef073ccdf7d19f89ce6674f6
auyer <rafael@rcpassos.me>
2021-02-18 16:11:08 -0300

    Folder structure, explicit memory clearing, LZ4 compression, update deps
    LZ4 issue fixed: https://github.com/apache/arrow/issues/3491

--- setup.py
+++ setup.py
@@ -2,22 +2,24 @@
 
 from setuptools import setup
 
-setup(name='target-parquet',
-      version='0.1.1',
-      description='Singer.io target for writing into parquet files',
-      author='Stitch',
-      url='https://singer.io',
-      classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['target_parquet'],
-      install_requires=[
-          'jsonschema==2.6.0',
-          'singer-python==5.9.0',
-          'pandas==1.1.3',
-          'pyarrow==1.0.1',
-          'psutil==5.7.2',
-      ],
-      entry_points='''
+setup(
+    name="target-parquet",
+    version="0.1.1",
+    description="Singer.io target for writing into parquet files",
+    author="Stitch",
+    url="https://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["target_parquet"],
+    install_requires=[
+        "jsonschema==2.6.0",
+        "singer-python==5.10",
+        "pandas==1.2",
+        "pyarrow==3.0",
+        "psutil==5.8",
+    ],
+    entry_points="""
           [console_scripts]
           target-parquet=target_parquet:main
-      ''',
-      )
+      """,
+    packages=["target_parquet"],
+)
