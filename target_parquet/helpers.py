975e221d13d1caf0f79da58d5d0d92957da13e5f
Jonas Amaro <jonas.amaro@estrategiaconcursos.com.br>
2021-06-09 18:33:48 -0300

    Adiciona função de achatamento de schema flatten_schema

--- target_parquet/helpers.py
+++ target_parquet/helpers.py
@@ -33,3 +33,50 @@ def flatten(dictionary, parent_key="", sep="__"):
         else:
             items.append((new_key, str(v) if type(v) is list else v))
     return dict(items)
+
+def flatten_schema(dictionary, parent_key="", sep="__"):
+    """Function that flattens a nested structure, using the separater given as parameter, or uses '__' as default
+    E.g:
+     dictionary =  {
+                        'key_1': {'type': ['null', 'integer']},
+                        'key_2': {
+                            'type': ['null', 'object'],
+                            'properties': {
+                                'key_3': {'type': ['null', 'string']},
+                                'key_4': {
+                                    'type': ['null', 'object'],
+                                    'properties': {
+                                        'key_5' : {'type': ['null', 'integer']},
+                                        'key_6' : {
+                                            'type': ['null', 'array'],
+                                            'items': {
+                                                'type': ['null', 'object'],
+                                                'properties': {
+                                                    'key_7': {'type': ['null', 'number']},
+                                                    'key_8': {'type': ['null', 'string']}
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+    By calling the function with the dictionary above as parameter, you will get the following strucure:
+        [
+             'key_1',
+             'key_2__key_3',
+             'key_2__key_4__key_5',
+             'key_2__key_4__key_6'
+        ]
+    """
+    items = []
+    for k, v in dictionary.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if v.get("type")[-1] == "object":
+            items.extend(flatten_schema(v.get("properties"),
+                                 new_key,
+                                 sep=sep))
+        else:
+            items.append(new_key)
+    return items
